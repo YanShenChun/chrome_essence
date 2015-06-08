d13ef06213015c8d3cefed7d7cff4151994645b7
yanshenchun <shanghaitan1987@126.com>
2015-06-08 16:06:26 +0800

    Add a small example to show the basic feature of chromium logging library

--- chrome_logging/chrome_logging/client.cc
+++ chrome_logging/chrome_logging/client.cc
@@ -1,7 +1,24 @@
 #include <stdio.h>
 #include <tchar.h>
+#include "base\logging.h"
+#include "base\command_line.h"
 
-int _tmain(int argc, _TCHAR* argv[])
+int main(int argc, const char* const argv[])
 {
+    base::CommandLine::Init(argc, argv);
+    logging::LoggingSettings settings;
+    settings.logging_dest = logging::LOG_TO_SYSTEM_DEBUG_LOG;
+    logging::InitLogging(settings);
+    
+    logging::SetLogItems(true, true, false, false);
+    
+    LOG(INFO) << "INFO";
+    LOG(WARNING) << "WARNING";
+    LOG(ERROR) << "ERROR";
+    
+    LOG_IF(INFO, true) << "LOG_IF INFO";
+    LOG_IF(WARNING, false) << "LOG_IF WARNING";
+    LOG_IF(ERROR, true) << "LOG_IF ERROR";
+
     return 0;
 }
